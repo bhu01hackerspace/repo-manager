cb695c9fafec221f9d4a50aacfe0aebaf16981cf
bhu01hackerspace <joao.asoliveira.pro@gmail.com>
2026-04-01 21:35:01 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Clone o repositório
+git clone https://github.com/bhu01hackerspace/repo-manager.git
+
+# Acesse o diretório
+cd repo-manager
+
+# Dê permissão de execução
+chmod +x repo-manager.sh
+
+# Execute
+./repo-manager.sh
